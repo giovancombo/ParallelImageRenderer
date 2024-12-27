50faf71398ab904f9faa6f69a71e796a9a030509
Giovanni Colombo <giovanni.colombo@edu.unifi.it>
2024-12-27 19:17:36 +0100

    Update renderer.cpp

--- src/renderer.cpp
+++ src/renderer.cpp
@@ -7,8 +7,7 @@
 using namespace std;
 
 Renderer::Renderer(int w, int h) : width(w), height(h) {
-    canvas = new Color[width * height];
-    // canvas = new PaddedColor[width * height];    to address False Sharing
+    canvas = new Color[width * height];         // canvas = new PaddedColor[width * height];    to address False Sharing
     for(int i = 0; i < width * height; i++) {
         canvas[i] = Color(1.0f, 1.0f, 1.0f);
         // canvas[i].r = 1.0f;
@@ -33,12 +32,12 @@ Color Renderer::alphaBlending(const Color& source, const Color& dest, float alph
     return Color(
             source.r * alpha + dest.r * (1 - alpha),
             source.g * alpha + dest.g * (1 - alpha),
-            source.b * alpha + dest.b * (1 - alpha));
+            source.b * alpha + dest.b * (1 - alpha)
+            );
 }
 
 void Renderer::processPixel(int x, int y) {
-    Color finalColor;
-    // Color finalColor(1.0f, 1.0f, 1.0f);      to address False Sharing
+    Color finalColor;       // Color finalColor(1.0f, 1.0f, 1.0f);      to address False Sharing
 
     for (const Circle& circle : circles) {
         if (isPixelInCircle(x, y, circle)) {
@@ -56,6 +55,20 @@ void Renderer::addCircle(const Circle& circle) {
     circles.push_back(circle);
 }
 
+void Renderer::saveToPPM(const string& filename) {
+    ofstream file(filename, ios::binary);
+    file << "P6\n" << width << " " << height << "\n255\n";
+
+    for(int i = 0; i < width * height; i++) {
+        unsigned char r = static_cast<unsigned char>(canvas[i].r * 255);
+        unsigned char g = static_cast<unsigned char>(canvas[i].g * 255);
+        unsigned char b = static_cast<unsigned char>(canvas[i].b * 255);
+        file.write(reinterpret_cast<char*>(&r), 1);
+        file.write(reinterpret_cast<char*>(&g), 1);
+        file.write(reinterpret_cast<char*>(&b), 1);
+    }
+}
+
 SequentialResult Renderer::renderSequential() {
     SequentialResult result;
 
@@ -63,6 +76,7 @@ SequentialResult Renderer::renderSequential() {
     auto startSort = chrono::high_resolution_clock::now();
     sort(circles.begin(), circles.end(),[](const Circle& c1, const Circle& c2) { return c1.z > c2.z; });
     auto endSort = chrono::high_resolution_clock::now();
+    result.seqSortingTime = chrono::duration_cast<chrono::milliseconds>(endSort - startSort).count();
 
     // 2. Blending colors for all pixels, checking whether a circle belongs to a pixel
     auto startRender = chrono::high_resolution_clock::now();
@@ -72,40 +86,37 @@ SequentialResult Renderer::renderSequential() {
         }
     }
     auto endRender = chrono::high_resolution_clock::now();
-
-    result.seqSortingTime = chrono::duration_cast<chrono::milliseconds>(endSort - startSort).count();
     result.seqRenderTime = chrono::duration_cast<chrono::milliseconds>(endRender - startRender).count();
+
     result.seqExecutionTime = result.seqSortingTime + result.seqRenderTime;
 
     return result;
 }
 
-ParallelResult Renderer::renderParallel(int num_threads, int block_size, float seq_execution_time) {
+ParallelResult Renderer::renderParallel(int numThreads, int blockSize, float seqExecutionTime) {
     ParallelResult result;
-
-    result.numThreads = num_threads;
-    result.blockSize = block_size;
+    result.numThreads = numThreads;
+    result.blockSize = blockSize;
 
     // 1. Sorting circles accordingly with their z coordinate
     auto startSort = chrono::high_resolution_clock::now();
     sort(circles.begin(), circles.end(),[](const Circle& c1, const Circle& c2) { return c1.z > c2.z; });
     auto endSort = chrono::high_resolution_clock::now();
+    result.parSortingTime = chrono::duration_cast<chrono::milliseconds>(endSort - startSort).count();
 
     // 2. Blending colors for all pixels, checking whether a circle belongs to a pixel
     auto startRender = chrono::high_resolution_clock::now();
-    #pragma omp parallel for num_threads(num_threads)
+    #pragma omp parallel for num_threads(numThreads)
         for (int y = 0; y < height; y++) {
             for (int x = 0; x < width; x++) {
                 processPixel(x, y);
             }
         }
     auto endRender = chrono::high_resolution_clock::now();
-
-    result.parSortingTime = chrono::duration_cast<chrono::milliseconds>(endSort - startSort).count();
     result.parRenderTime = chrono::duration_cast<chrono::milliseconds>(endRender - startRender).count();
-    result.parExecutionTime = result.parSortingTime + result.parRenderTime;
 
-    result.speedup = seq_execution_time/result.parExecutionTime;
+    result.parExecutionTime = result.parSortingTime + result.parRenderTime;
+    result.speedup = seqExecutionTime/result.parExecutionTime;
     result.efficiency = result.speedup/result.numThreads;
 
     return result;
